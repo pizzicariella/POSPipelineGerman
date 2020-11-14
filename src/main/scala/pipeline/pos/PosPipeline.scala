204a806c8db85a43b37b7e53040bac53cff39d4b
Marie Kemker <s0558478@htw-berlin.de>
2020-11-14 14:55:09 +0100

    workaround for tokenizer

--- src/main/scala/pipeline/pos/PosPipeline.scala
+++ src/main/scala/pipeline/pos/PosPipeline.scala
@@ -4,7 +4,7 @@ import com.johnsnowlabs.nlp.annotators.pos.perceptron.PerceptronModel
 import com.johnsnowlabs.nlp.annotator.{LemmatizerModel, Normalizer, SentenceDetector, Tokenizer}
 import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.sql.functions.{col, regexp_replace}
+import org.apache.spark.sql.functions.{col, regexp_extract, regexp_replace}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import pipeline.PipelineTrait
 
@@ -26,12 +26,17 @@ class PosPipeline(val spark: SparkSession) extends PipelineTrait{
   val tokenizer = new Tokenizer()
     .setInputCols(Array("sentence"))
     .setOutputCol("token")
-    //.addSplitChars(" ")
-    .setSuffixPattern("([^\\s\\w\\ü\\ö\\ä\\ß]?)([^\\s\\w\\ü\\ö\\ä\\ß]*)\\z")
+    //.addSplitChars("\\.")
+    .setSuffixPattern("([^\\s\\w\\ü\\ö\\ä\\ß\\-]?)([^\\s\\w\\ü\\ö\\ä\\ß\\-]*)\\z")
     //.setSplitPattern("[ ]")
     //.setContextChars(Array("\"", ".", ",", "!", "?", ":"))
     //.setInfixPatterns(Array("([^\\s\\w\\ü\\ö\\ä\\ß\\-]?)", "([^\\s\\w\\ü\\ö\\ä\\ß\\-]*)"))
+    //.setInfixPatterns(Array("([\\p{L}\\w]\\.{1})([\\p{L}\\w])"))
+    //.setInfixPatterns(Array("((?:\\p{L}\\.)+)"))
+    //.setInfixPatterns(Array("((?:\\p{L}+[^\\s\\p{L}]{1})+\\p{L}+)"))
     //.setPrefixPattern("\\A([^\\s\\w]*)")
+
+  println("explaination: "+tokenizer.explainParams())
 
   /*val normalizer = new Normalizer()
     .setInputCols(Array("token"))
@@ -67,19 +72,25 @@ class PosPipeline(val spark: SparkSession) extends PipelineTrait{
                             replacement: Option[String]): DataFrame = {
     val data = articles.toDF("text")
     val dataEdited = replaceSplitChars(data, replacePatternSplitChars, replacement)
-    pipeline.fit(dataEdited).transform(dataEdited)
+    val dataEdited2 = preEditText(dataEdited)
+    pipeline.fit(dataEdited2).transform(dataEdited2)
   }
 
   def replaceSplitChars(articlesDf: DataFrame,
                         replacePatternSplitChars: Option[String],
                         replacement: Option[String]): DataFrame = {
     articlesDf.withColumn("text", replacePatternSplitChars match {
-      case Some(x) => regexp_replace(articlesDf("text"), x, replacement match {
-        case Some(x) => x
+      case Some(value) => regexp_replace(articlesDf("text"), value, replacement match {
+        case Some(value) => value
         case _ => " "
       })
       case _ => col("text")
     })
+  }
+
+  //TODO refactor and change structur. Possible to create spark nlp annotator for text preprocessing?
+  def preEditText(articlesDf: DataFrame): DataFrame = {
+    articlesDf.withColumn("text", regexp_replace(articlesDf("text"), "\\.", ". "))
   }
 
 }
