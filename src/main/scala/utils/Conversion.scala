0ab2637fce50ba217112eb480a120a794835c1b4
Marie Kemker <s0558478@htw-berlin.de>
2020-12-02 15:28:39 +0100

    refactoring tests pt 1

--- src/main/scala/utils/Conversion.scala
+++ src/main/scala/utils/Conversion.scala
@@ -4,25 +4,6 @@ import model.{NewsArticle, Strings}
 
 object Conversion {
 
-  //TODO how to handle non string columns?
-  def getArticleWithCompleteText(articleMap: Map[String, Any],
-                                 textColumns: Array[String],
-                                 otherRelevantColumns: Array[String]): (Array[String], String) = {
-
-    //TODO add special char to distinguish title intro and text?
-    val text = textColumns.map(columnName =>
-      articleMap.getOrElse(columnName,
-        throw new NoSuchElementException(Strings.noSuchColumnString(columnName))) match {
-        case s: String => s
-        case _ => Strings.empty
-      })
-      .reduce((s1, s2) => s1 + Strings.whitespace + s2)
-
-    (otherRelevantColumns.map(columnName =>
-      articleMap.getOrElse(columnName,
-        throw new NoSuchElementException(Strings.noSuchColumnString(columnName))).toString), text)
-  }
-
   def prepareArticles(articles: Seq[NewsArticle]): Seq[(String, String, String, String)] = {
     articles.map(article => Conversion.switchArticleFormat(article))
   }
---
+++ src/test/scala/utils/ConversionTest.scala
@@ -0,0 +1,36 @@
+package utils
+
+import com.typesafe.config.ConfigFactory
+import model.NewsArticle
+import org.scalatest.funsuite.AnyFunSuite
+import model.Strings
+import utils.json.JsonParser
+
+class ConversionTest extends AnyFunSuite{
+
+  val testArticle = NewsArticle("test_1",
+    "www.test.de",
+    "1574281189000",
+    "Das Landeskriminalamt (LKA) und die Münchner Polizei",
+    "fanden in der mit Chemikalien vollgepackten Dreizimmerwohnung in Sichtweite des Gondrellplatzes",
+    "nämlich bisher mehr als 50 Kilogramm pyrotechnische Gegenstände und eine Vielzahl laut LKA vermutlich illegaler Feuerwerkskörper.")
+
+  val correctlyConverted = ("test_1",
+    "www.test.de",
+    "1574281189000",
+    "Das Landeskriminalamt (LKA) und die Münchner Polizei fanden in der mit Chemikalien vollgepackten Dreizimmerwohnung in Sichtweite des Gondrellplatzes nämlich bisher mehr als 50 Kilogramm pyrotechnische Gegenstände und eine Vielzahl laut LKA vermutlich illegaler Feuerwerkskörper.")
+
+  val newsArticles = FileReader.readJsonFile(ConfigFactory.load().getString(Strings.configTestFile))
+    .map(json => JsonParser.parseNewsArticle(json))
+
+  test("switchArticleFormat should convert NewsArticle correctly"){
+    val converted = Conversion.switchArticleFormat(testArticle)
+    assert(converted === correctlyConverted)
+  }
+
+  test("prepareArticles should map Seq of NewsArticles correctly"){
+    val convertedArticles = Conversion.prepareArticles(newsArticles)
+    assert(convertedArticles.isInstanceOf[Seq[(String, String, String, String)]])
+  }
+
+}
